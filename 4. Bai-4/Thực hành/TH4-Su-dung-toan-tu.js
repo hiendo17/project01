2d75ec4ffaf25347248d0a03d22d25b61a008969
Hien Do <hiendo.1759@gmail.com>
2021-12-30 17:25:49 +0700

    Commit

---
+++ 4. Bai-4/Thực hành/TH4-Su-dung-toan-tu.js
@@ -0,0 +1,4 @@
+    let width = Number(prompt("Enter the width"));
+    let height = Number(prompt("Enter the height"));
+    let area = width * height; 
+    document.write("The area is: " + area);
